0f387bf25eaf73acb990380cc14b0351e4de6109
Denise Alvaran <denisealvaran@gmail.com>
2026-04-22 13:42:58 +0800

    completed profile-section-bio for briefcase

--- Briefcase/pages/view/profile-section-bio.php
+++ Briefcase/pages/view/profile-section-bio.php
@@ -1,11 +1,28 @@
-<div class="card card-flush mb-5 mb-xl-8">
-    <div class="card-header pt-5">
-        <h3 class="card-title align-items-start flex-column">
-            <span class="card-label fw-bold text-gray-800 fs-3">Personal Information</span>
-            <span class="text-muted mt-1 fw-semibold fs-7">Short Biography</span>
-        </h3>
+
+<div class="card mb-5 mb-xl-10" id="kt_profile_details_view">
+    <div class="card-header border-0">
+        <!--begin::Card title-->
+        <div class="card-title m-0">
+            <h3 class="fw-bold fs-2 m-0">Profile Infromation</h3>
+        </div>
+        <!--end::Card title-->
+        <!--begin::Action-->
+        <a href="account/settings.html" class="btn btn-icon btn-sm btn-active-light-primary align-self-center">
+            <i class="ki-duotone ki-pencil fs-2">
+                <span class="path1"></span>
+                <span class="path2"></span>
+            </i>
+        </a>
+        <!--end::Action-->
     </div>
-    <div class="card-body">
-        <p class="text-gray-600 fs-6">I am a dedicated BSITBA student focused on data-driven decision making and business analytics.</p>
+    <!--begin::Card body-->
+    <div class="card-body p-9 pt-0">
+        <div class="mb-2">
+            <span class="fw-bold text-gray-800 fs-4">Short Biography</span>
+        </div>
+        <div class="text-gray-600 fw-semibold fs-5 lh-lg">
+            I am a dedicated and ambitious student of FEU Institute of Technology, pursuing a degree in Information Technology with a specialization in Business Analytics. With a passion for data-driven decision-making and a thirst for knowledge, I am eager to make a significant impact in the field of IT and business analytics.
+        </div>
     </div>
+    <!--end::Card body-->
 </div>
