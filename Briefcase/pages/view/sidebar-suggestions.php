94604041948e3f5c34a53e2da1c61bd3cd283b0f
Denise Alvaran <denisealvaran@gmail.com>
2026-04-22 15:16:43 +0800

    completed sidebar-suggestions for briefcase

---
+++ Briefcase/pages/view/sidebar-suggestions.php
@@ -0,0 +1,140 @@
+<!--begin::End sidebar-->
+<div class="card mb-5 mb-xl-10" id="kt_profile_details_view">
+    <div class="card-header border-0">
+        <!--begin::Header-->
+        <div class="card-header border-0 pt-5">
+            <h3 class="card-title align-items-start flex-column">
+                <span class="card-label fw-bold text-gray-900">Other <span class="text-primary fw-bold">BSITBA</span> Tamaraws</span>
+            </h3>
+            <!--begin::Toolbar-->
+            <div class="card-toolbar">
+                <!--begin::Menu-->
+                <button class="btn btn-icon btn-color-gray-500 btn-active-color-primary justify-content-end" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end" data-kt-menu-overflow="true">
+                    <i class="ki-duotone ki-dots-square fs-1">
+                        <span class="path1"></span>
+                        <span class="path2"></span>
+                        <span class="path3"></span>
+                        <span class="path4"></span>
+                    </i>
+                </button>
+            </div>
+            <!--end::Toolbar-->
+        </div>
+        <!--end::Header-->
+        <!--begin::Body-->
+        <div class="card-body pt-5">
+            <!--begin::Item-->
+            <div class="d-flex flex-stack">
+                <!--begin::Symbol-->
+                <div class="symbol symbol-40px me-5">
+                    <img src="assets/images/avatar.jpg" class="h-50 align-self-center" alt="" />
+                </div>
+                <!--end::Symbol-->
+                <!--begin::Section-->
+                <div class="d-flex align-items-center flex-row-fluid flex-wrap">
+                    <!--begin:Author-->
+                    <div class="flex-grow-1 me-2">
+                        <a href="pages/user-profile/overview.html" class="text-gray-800 text-hover-primary fs-6 fw-bold">Jacob Jones</a>
+                        <span class="text-muted fw-semibold d-block fs-7">BSITBA Student at FEU Institute of Technology</span>
+                    </div>
+                </div>
+                <!--end::Section-->
+            </div>
+            <!--end::Item-->
+            <!--begin::Separator-->
+            <div class="separator separator-dashed my-4"></div>
+            <!--end::Separator-->
+            <!--begin::Item-->
+            <div class="d-flex flex-stack">
+                <!--begin::Symbol-->
+                <div class="symbol symbol-40px me-5">
+                    <img src="assets/images/avatar.jpg" class="h-50 align-self-center" alt="" />
+                </div>
+                <!--end::Symbol-->
+                <!--begin::Section-->
+                <div class="d-flex align-items-center flex-row-fluid flex-wrap">
+                    <!--begin:Author-->
+                    <div class="flex-grow-1 me-2">
+                        <a href="pages/user-profile/overview.html" class="text-gray-800 text-hover-primary fs-6 fw-bold">Annette Black</a>
+                        <span class="text-muted fw-semibold d-block fs-7">BSITBA Student at FEU Institute of Technology</span>
+                    </div>
+                    <!--end:Author-->
+                </div>
+                <!--end::Section-->
+            </div>
+            <!--end::Item-->
+            <!--begin::Separator-->
+            <div class="separator separator-dashed my-4"></div>
+            <!--end::Separator-->
+            <!--begin::Item-->
+            <div class="d-flex flex-stack">
+                <!--begin::Symbol-->
+                <div class="symbol symbol-40px me-5">
+                    <img src="assets/images/avatar.jpg" class="h-50 align-self-center" alt="" />
+                </div>
+                <!--end::Symbol-->
+                <!--begin::Section-->
+                <div class="d-flex align-items-center flex-row-fluid flex-wrap">
+                    <!--begin:Author-->
+                    <div class="flex-grow-1 me-2">
+                        <a href="pages/user-profile/overview.html" class="text-gray-800 text-hover-primary fs-6 fw-bold">Devon Lane</a>
+                        <span class="text-muted fw-semibold d-block fs-7">BSITBA Student at FEU Institute of Technology</span>
+                    </div>
+                    <!--end:Author-->
+                </div>
+                <!--end::Section-->
+            </div>
+            <!--end::Item-->
+            <!--begin::Separator-->
+            <div class="separator separator-dashed my-4"></div>
+            <!--end::Separator-->
+            <!--begin::Item-->
+            <div class="d-flex flex-stack">
+                <!--begin::Symbol-->
+                <div class="symbol symbol-40px me-5">
+                    <img src="assets/images/avatar.jpg" class="h-50 align-self-center" alt="" />
+                </div>
+                <!--end::Symbol-->
+                <!--begin::Section-->
+                <div class="d-flex align-items-center flex-row-fluid flex-wrap">
+                    <!--begin:Author-->
+                    <div class="flex-grow-1 me-2">
+                        <a href="pages/user-profile/overview.html" class="text-gray-800 text-hover-primary fs-6 fw-bold">Kristin Watson</a>
+                        <span class="text-muted fw-semibold d-block fs-7">BSITBA Student at FEU Institute of Technology</span>
+                    </div>
+                    <!--end:Author-->
+                </div>
+                <!--end::Section-->
+            </div>
+            <!--end::Item-->
+            <!--begin::Separator-->
+            <div class="separator separator-dashed my-4"></div>
+            <!--end::Separator-->
+            <!--begin::Item-->
+            <div class="d-flex flex-stack">
+                <!--begin::Symbol-->
+                <div class="symbol symbol-40px me-5">
+                    <img src="assets/images/avatar.jpg" class="h-50 align-self-center" alt="" />
+                </div>
+                <!--end::Symbol-->
+                <!--begin::Section-->
+                <div class="d-flex align-items-center flex-row-fluid flex-wrap">
+                    <!--begin:Author-->
+                    <div class="flex-grow-1 me-2">
+                        <a href="pages/user-profile/overview.html" class="text-gray-800 text-hover-primary fs-6 fw-bold">Eleanor Pena</a>
+                        <span class="text-muted fw-semibold d-block fs-7">BSITBA Student at FEU Institute of Technology</span>
+                    </div>
+                </div>
+            </div>
+            <div class="separator separator-dashed my-4"></div>
+            <div class="text-center pt-5">
+                <a href="#" class="btn btn-sm btn-light-primary w-100 fw-bold">
+                    View All Tamaraws
+                </a>
+            </div>
+            <!--end::Item-->
+        </div>
+        <!--end::Body-->
+    </div>
+</div>
+<!--end::End sidebar-->
