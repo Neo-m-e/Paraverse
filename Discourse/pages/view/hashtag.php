597fb35404020e4736037586b9b0ebc853c78358
Denise Alvaran <denisealvaran@gmail.com>
2026-06-03 11:02:09 +0800

    added comment on comment feature

--- Discourse/pages/view/hashtag.php
+++ Discourse/pages/view/hashtag.php
@@ -91,7 +91,6 @@ $META_TITLE = $tagDisplay . ' — Discourse Hashtag';
 <body id="kt_app_body" data-kt-app-page-loading-enabled="true" data-kt-app-page-loading="on"
     data-kt-app-layout="light-header" class="app-default">
 
-    <?php include(dirname(dirname(__DIR__)) . "/partials/_page-loader.php"); ?>
 
     <div class="d-flex flex-column flex-root app-root" id="kt_app_root">
         <div class="app-page flex-column flex-column-fluid" id="kt_app_page">
